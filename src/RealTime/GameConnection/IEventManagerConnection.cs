32f4685377e61a85291fb6aea3fdb5cf64681b2e
Tal <talrofeh@gmail.com>
2026-04-13 23:13:07 +0300

    fix

--- src/RealTime/GameConnection/IEventManagerConnection.cs
+++ src/RealTime/GameConnection/IEventManagerConnection.cs
@@ -11,7 +11,7 @@ namespace RealTime.GameConnection
         /// <summary>Gets the flags of an event with specified ID.</summary>
         /// <param name="eventId">The ID of the event to get flags of.</param>
         /// <returns>The event flags or <see cref="EventData.Flags.None"/> if none found.</returns>
-        EventData.Flags GetEventFlags(ushort eventId);
+        internal EventData.Flags GetEventFlags(ushort eventId);
 
         /// <summary>
         /// Gets a collection of the IDs of upcoming city events in the specified time interval.
@@ -19,28 +19,28 @@ namespace RealTime.GameConnection
         /// <param name="earliestTime">The start time of the interval to get events from.</param>
         /// <param name="latestTime">The end time of the interval to get events from.</param>
         /// <returns>A collection of the city event IDs.</returns>
-        IReadOnlyList<ushort> GetUpcomingEvents(DateTime earliestTime, DateTime latestTime);
+        internal IReadOnlyList<ushort> GetUpcomingEvents(DateTime earliestTime, DateTime latestTime);
 
         /// <summary>Gets various information about a city event with specified ID.</summary>
         /// <param name="eventId">The ID of the city event to get information for.</param>
         /// <param name="eventInfo">A <see cref="VanillaEventInfo"/> ref-struct containing the event information.</param>
         /// <returns><c>true</c> if the information was retrieved; otherwise, <c>false</c>.</returns>
-        bool TryGetEventInfo(ushort eventId, out VanillaEventInfo eventInfo);
+        internal bool TryGetEventInfo(ushort eventId, out VanillaEventInfo eventInfo);
 
         /// <summary>Gets the start time of a city event with specified ID.</summary>
         /// <param name="eventId">The ID of the city event to get start time of.</param>
         /// <param name="startTime">The start time of the event with the specified ID.</param>
         /// <returns><c>true</c> if the start time was retrieved; otherwise, <c>false</c>.</returns>
-        bool TryGetEventStartTime(ushort eventId, out DateTime startTime);
+        internal bool TryGetEventStartTime(ushort eventId, out DateTime startTime);
 
         /// <summary>Gets the color of a city event with specified ID.</summary>
         /// <param name="eventId">The ID of the city event to get the color of.</param>
         /// <returns>The color of the event.</returns>
-        EventColor GetEventColor(ushort eventId);
+        internal EventColor GetEventColor(ushort eventId);
 
         /// <summary>Sets the start time of the event to the specified value.</summary>
         /// <param name="eventId">The ID of the event to change.</param>
         /// <param name="startTime">The new event start time.</param>
-        void SetStartTime(ushort eventId, DateTime startTime);
+        internal void SetStartTime(ushort eventId, DateTime startTime);
     }
 }
